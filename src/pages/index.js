1f387c5bfbdf3d7364cc0df11499560cd5cb1609
Madhav Bahl <madhavbahl10@gmail.com>
2018-09-06 11:10:22 +0530

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>This is Madhav Bahl</h1>
+    <img src="https://avatars0.githubusercontent.com/u/26179770?s=460&v=4" />
+    <p>A Cadet Who Codes | Open Source Enthusiast | Full Stack Web Developer | Making Impossible Possible</p>
+    <p>Looking forward to build something great.</p>
+    
+  </div>
+)
+
+export default IndexPage
